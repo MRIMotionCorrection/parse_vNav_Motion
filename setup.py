7f33b667787aa30c9cbdd0a1f8176dc339929bdf
Haley Acevedo <haley.acevedo2@gmail.com>
2019-02-14 08:14:26 -0600

    fixed setup.py dependencies/url

--- setup.py
+++ setup.py
@@ -3,9 +3,9 @@ from setuptools import setup
 setup(name='vnav',
     version='0.1',
     description='Script to parse DICOM files from a vNav series and convert them into motion scores',
-    url='https://github.com/hacevedo/parse_vNav_Motion',
+    url='https://github.com/MRIMotionCorrection/parse_vNav_Motion',
     author='Dylan Tisdall',
     author_email='mtisdall@mail.med.upenn.edu',
     license='MIT',
     packages=['vnav'],
-    install_requires=['pydicom'])
+    install_requires=['pydicom', 'numpy'])
